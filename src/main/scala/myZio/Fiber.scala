f7e383dc6d43daeb8047bcd12f7276de3b47dd63
Anssi Pitkänen <anssi.pitkan@gmail.com>
2022-09-30 17:42:14 +0300

    Fiber concurrency safety

--- src/main/scala/myZio/Fiber.scala
+++ src/main/scala/myZio/Fiber.scala
@@ -1,5 +1,6 @@
 package myZio
 
+import java.util.concurrent.atomic.AtomicReference
 import scala.concurrent.ExecutionContext
 
 sealed trait Fiber[+A] {
@@ -11,28 +12,57 @@ sealed trait Fiber[+A] {
 
 class FiberImpl[A](zio: ZIO[A]) extends Fiber[A] {
 
-  var maybeResult: Option[A] = None
+  sealed trait FiberState
+  final case class Running(callbacks: List[A => Any]) extends FiberState
+  final case class Done(result: A) extends FiberState
 
-  var callbacks = List.empty[A => Any]
+  var state: AtomicReference[FiberState] =
+    new AtomicReference(Running(List.empty))
+
+  def await(callback: A => Any): Unit = {
+    var loop = true
+    while (loop) {
+      val oldState = state.get()
+      oldState match {
+
+        case Running(callbacks) =>
+          val newState = Running(callback :: callbacks)
+          loop = !state.compareAndSet(oldState, newState)
+
+        case Done(result) =>
+          callback(result)
+          loop = false
+      }
+    }
+  }
+
+  def complete(result: A): Unit = {
+    var loop = true
+    while (loop) {
+      val oldState = state.get()
+      oldState match {
+
+        case Running(callbacks) =>
+          if (state.compareAndSet(oldState, Done(result))) {
+            loop = false
+            callbacks.foreach { cb =>
+              cb(result)
+            }
+          }
+
+        case Done(_) =>
+          throw new Exception("Fiber completed more than once")
+      }
+    }
+  }
 
   override def join: ZIO[A] =
-    maybeResult match {
-      case None =>
-        ZIO.async { complete =>
-          callbacks = complete :: callbacks
-        }
-
-      case Some(result) =>
-        ZIO.succeedNow(result)
+    ZIO.async { callback =>
+      await(callback)
     }
 
   override def start(): Unit =
     ExecutionContext.global.execute { () =>
-      zio.run { result =>
-        maybeResult = Some(result)
-        callbacks.foreach { callback =>
-          callback(result)
-        }
-      }
+      zio.run(complete)
     }
 }
